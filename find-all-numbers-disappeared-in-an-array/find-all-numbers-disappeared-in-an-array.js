449d8c67ab6ec7fa487c618b40724c3794c5a26e
Minchan Lee <shigatsu970704@gmail.com>
2021-11-13 15:50:11 +0900

    Time: 96 ms (98.76%), Space: 50.7 MB (24.28%) - LeetHub

---
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let length = nums.length
+    let arr = []
+    for (let i = 0; i < length; i++) {
+      arr[i] = i+1
+    }
+    for (v of nums) {
+      arr[v-1] = -1
+    }
+    return arr.filter(i => (i > 0))
+};
