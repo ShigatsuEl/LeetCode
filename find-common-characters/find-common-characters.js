21fbad07472910a2e6793c63e8e31edb79c2a5c1
Minchan Lee <shigatsu970704@gmail.com>
2021-12-19 14:53:22 +0900

    Time: 76 ms (97.54%), Space: 40.8 MB (85.56%) - LeetHub

---
+++ find-common-characters/find-common-characters.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function(words) {
+    const result = [];
+    const firstWordArr = [...words[0]];
+    
+    for(const letter of firstWordArr) {
+        if(words.every(word => word.includes(letter))) {
+            result.push(letter);
+            words = words.map(word => word.replace(letter, ''));
+        }
+    }    
+    
+    return result;
+};
