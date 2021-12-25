944d1c6caaf20a5e995c5c1019319b54d6f3a444
Minchan Lee <shigatsu970704@gmail.com>
2021-12-25 11:10:39 +0900

    Time: 76 ms (71.96%), Space: 40.3 MB (54.21%) - LeetHub

---
+++ last-stone-weight/last-stone-weight.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    let size = stones.length;
+     
+    while(size >= 2){
+        stones.sort((a,b)=> a - b);
+        
+        x = stones.pop(); 
+        y = stones.pop();
+        
+        if(x == y){
+            size -= 2;
+        }else{
+            size -= 1; 
+            stones.unshift(x - y); 
+        }
+    }
+    
+    return stones;
+};
