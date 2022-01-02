2c8b9527f552efd45d93edd3e39e09523a373ed0
Minchan Lee <shigatsu970704@gmail.com>
2022-01-02 14:20:34 +0900

    Time: 509 ms (45.42%), Space: 120.1 MB (5.34%) - LeetHub

---
+++ finding-3-digit-even-numbers/finding-3-digit-even-numbers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var findEvenNumbers = function(digits) {
+    let arr = [];
+    
+    for(let i = 0; i < digits.length; i++) {
+        if (digits[i] % 2 == 0) {
+            for(let j = 0;j < digits.length; j++) {
+                if(i !== j && digits[j] !== 0) { // exclude the hundreds digit which is zero
+                    for(let k = 0; k < digits.length; k++) {
+                        if(k !== i && k !== j){
+                            arr.push(digits[j] * 100 + digits[k] * 10 + digits[i]);
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    let unique = [...new Set(arr)];
+    return unique.sort();
+};
