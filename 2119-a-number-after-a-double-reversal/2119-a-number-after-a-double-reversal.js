40bfdbd30493d7b96f6d8be03c4e1bf3b1169692
Minchan Lee <shigatsu970704@gmail.com>
2022-02-20 19:19:26 +0900

    Time: 52 ms (99.73%), Space: 42.5 MB (7.39%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function(num) {
+    if (num === 0) return true;
+    else {
+        if (num % 10 !== 0) return true;
+        else return false;
+    }
+};
