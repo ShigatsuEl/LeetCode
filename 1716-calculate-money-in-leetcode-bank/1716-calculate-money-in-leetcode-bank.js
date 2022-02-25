42417230dbd0b97bc93c38c18438ba58e20657fc
Minchan Lee <shigatsu970704@gmail.com>
2022-02-25 18:49:58 +0900

    Time: 79 ms (59.37%), Space: 41.8 MB (37.50%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalMoney = function(n) {
+    let countMonday = Math.floor(n / 7);
+    let seq = ((countMonday ** 2) + (7 * countMonday)) / 2;
+    let sum = seq * 7;
+    for(let i = 0; i < n - (countMonday * 7); i++) {
+        sum += countMonday + i + 1;
+    }
+    return sum;
+};
+
