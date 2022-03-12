223e888309bdd4c53cb0d6f50a93d5cb39b24cbd
Minchan Lee <shigatsu970704@gmail.com>
2022-03-12 17:19:54 +0900

    Time: 98 ms (45.25%), Space: 42.6 MB (38.46%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    
+    n = n.toString(2);
+    
+    let result = 0;
+    
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] === '1') result++;
+    }
+    
+    return result;
+};
